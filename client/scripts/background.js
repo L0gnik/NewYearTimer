34d077a3a65fe410ffd14650cea00b681a428e17
L0gnik <nik.logvin92@gmail.com>
2022-12-05 17:55:49 +0300

    commit

--- client/scripts/background.js
+++ client/scripts/background.js
@@ -1,5 +1,4 @@
-export default backgroundImg = () => {
-
-    console.log(123);
-    
-}
+const img = document.getElementsByClassName('bg')[0]
+const imgSrc = img.getAttribute('src').toString();
+img.removeAttribute('src');
+img.setAttribute('style', `background-image: url(${imgSrc});`)
