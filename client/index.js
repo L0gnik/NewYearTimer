cd35da7e7bf412c85fb092a6b0ae95332c8c8eee
L0gnik <nik.logvin92@gmail.com>
2022-12-08 13:04:14 +0300

    commmit

--- client/index.js
+++ client/index.js
@@ -1,3 +1,12 @@
-import { background } from './scripts/background.js';
 
-background();
+    let timeNow = new Date().getTime();
+    const countDownDate = new Date("Jun 1, 2023 0:00:00").getTime();
+    let timeLeft = countDownDate - timeNow;
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
+    
+    var myfunc = setInterval(function () {
+       
+        console.log(document.getElementsByClassName('newYearTimer'));
+        
+    }, 1000)
+    document.getElementsByClassName('newYearTimer').innerHTML = days + 'd'
